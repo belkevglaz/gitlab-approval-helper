104f79b738951fc92ec354bae32e2b9b6e16d1b9
Aksenov Ivan <belkevglaz@gmail.com>
2022-12-19 14:39:14 +0300

    Initial commit

---
+++ src/main/kotlin/voneska/ru/Application.kt
@@ -0,0 +1,12 @@
+package voneska.ru
+
+import io.ktor.server.application.*
+import voneska.ru.plugins.*
+
+fun main(args: Array<String>): Unit =
+	io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+	configureRouting()
+}
